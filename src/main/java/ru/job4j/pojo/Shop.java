358f1929f15c584e5bd0624d92074f196e46d269
Konstantin <89169555954@mail.ru>
2024-11-28 00:28:15 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,7 +2,7 @@ package ru.job4j.pojo;
 
 public class Shop {
 
-    public static int indexOfNull(product[] products) {
+    public static int indexOfNull(Product[] products) {
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (products[i] == null) {
