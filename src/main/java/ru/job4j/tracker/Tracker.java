d138943ce55b0f299d92dbd2d24a16045f334a2b
Konstantin <89169555954@mail.ru>
2024-12-10 23:52:11 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,25 +1,60 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+    private final List<Item> items = new ArrayList<>();
 
     public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+        int index = indexOf(id);
+        return index != -1 ? items.get(index) : null;
+    }
+
+    public Item[] findAll() {
+        return items.toArray(new Item[0]);
+    }
+
+    public Item[] findByName(String name) {
+        List<Item> result = new ArrayList<>();
+        for (Item item : items) {
+            if (item.getName().equals(name)) {
+                result.add(item);
             }
         }
-        return rsl;
+        return result.toArray(new Item[0]);
+    }
+
+    public boolean replace(int id, Item newItem) {
+        int index = indexOf(id);
+        if (index != -1) {
+            newItem.setId(id); // Сохраняем старый id
+            items.set(index, newItem);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items.remove(index); // Удаление элемента по индексу
+            return true;
+        }
+        return false;
+    }
+
+    private int indexOf(int id) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
