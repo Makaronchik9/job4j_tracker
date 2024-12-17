47d2451bdf73c0fc8642afb2b8bd7000e55715f8
Konstantin <89169555954@mail.ru>
2024-12-18 01:24:21 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -3,14 +3,12 @@ package ru.job4j.polymorphism;
 public class Bus implements Transport {
     @Override
     public void move() {
-        System.out.println("Автобус приедет.");
+        System.out.println("Автобус едет.");
     }
-
     @Override
     public void passengers(int count) {
         System.out.println("Количество пассажиров: " + count);
     }
-
     @Override
     public int refuel(int fuel) {
         int fuelPrice = 50;
