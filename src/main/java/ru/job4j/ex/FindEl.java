1d77ec6425e08ffd2eed268c37011d635de28eb5
Konstantin <89169555954@mail.ru>
2025-02-03 23:54:55 +0300

    Репозитарий под проект Tracker

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+                break;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException();
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        try {
+            String[] array = {"Sunday", "Monday", "Wednesday", "Thursday"};
+            String key = "Monday";
+            int index = indexOf(array, key);
+            System.out.println("Index of " + key + " is " + index);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
