036ea6333435b360525d6ec072b92f7e3b05b17d
Konstantin <89169555954@mail.ru>
2025-02-04 22:22:26 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,9 +2,12 @@ package ru.job4j.inheritance;
 
 public class JSONReport {
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator()
-                + "\t\"name\" : \"" + name + "\"," + System.lineSeparator()
-                + "\t\"body\" : \"" + body + "\"" + System.lineSeparator()
+        return "{"
+                + System.lineSeparator()
+                + "\t\"name\" : \"" + name + "\","
+                + System.lineSeparator()
+                + "\t\"body\" : \"" + body + "\""
+                + System.lineSeparator()
                 + "}";
     }
 }
