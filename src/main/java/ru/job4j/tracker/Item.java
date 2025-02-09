6e3b313fa8ce4a1179a8b539a7c07c58e14efc68
Konstantin <89169555954@mail.ru>
2025-02-09 22:25:18 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,28 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
     private int id;
     private String name;
+    private LocalDateTime created;
 
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Item(int id, String name, LocalDateTime created) {
         this.id = id;
+        this.name = name;
+        this.created = created;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
