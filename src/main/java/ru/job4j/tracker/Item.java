518c0369ba93691e072a7c5d0ce2e27c6cdfb6c5
Konstantin <89169555954@mail.ru>
2025-02-10 00:27:44 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,23 +7,19 @@ public class Item {
     private int id;
     private String name;
 
-    // Конструктор
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    // Геттер для ID
     public int getId() {
         return id;
     }
 
-    // Геттер для имени
     public String getName() {
         return name;
     }
 
-    // Сеттер для ID
     public void setId(int id) {
         this.id = id;
     }
