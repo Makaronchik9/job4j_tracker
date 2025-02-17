d0ec7bfbbd058a372c660572d570be4cfc982ddd
Konstantin <89169555954@mail.ru>
2025-02-17 23:24:56 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/ooa/Airport.java
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -2,7 +2,7 @@ package ru.job4j.ooa;
 
 public class Airport {
     public static void main(String[] args) {
-        Airbus airbus = new Airbus("A320");
+        Airbus airbus = new Airbus("A380");
         Airbus airbus1 = new Airbus("A321");
         airbus.printModel();
         airbus.printCountEngine();
