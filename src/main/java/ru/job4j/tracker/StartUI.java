0bf38a82bc9539b17858660cbf2dfe06ecf147e5
Konstantin <89169555954@mail.ru>
2025-02-18 23:32:51 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,41 +1,30 @@
 package ru.job4j.tracker;
 
 public class StartUI {
-    private final Output out;
+    private final Output output;
 
-    public StartUI(Output out) {
-        this.out = out;
+    public StartUI(Output output) {
+        this.output = output;
     }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
             showMenu(actions);
-            int select = input.askInt("Select: ");
+            int select = input.askInt("Выберите: ");
+            if (select < 0 || select >= actions.length) {
+                output.println("Ошибка: некорректный ввод. Введите число от 0 до " + (actions.length - 1));
+                continue;
+            }
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
     }
 
     private void showMenu(UserAction[] actions) {
-        out.println("Menu.");
-        for (int i = 0; i < actions.length; i++) {
-            out.println(i + ". " + actions[i].name());
+        output.println("Меню:");
+        for (int index = 0; index < actions.length; index++) {
+            output.println(index + ". " + actions[index].name());
         }
-    }
-
-    public static void main(String[] args) {
-        Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(output),
-                new ShowAllAction(output),
-                new EditAction(output),
-                new DeleteAction(output),
-                new FindItemByNameAction(output),
-                new ExitAction()
-        };
-        new StartUI(output).init(input, tracker, actions);
     }
 }
--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,12 +1,12 @@
 package ru.job4j.tracker;
 
 public class ValidateInput implements Input {
-    private final Output output;
     private final Input input;
+    private final Output output;
 
     public ValidateInput(Output output, Input input) {
-        this.output = output;
         this.input = input;
+        this.output = output;
     }
 
     @Override
@@ -16,15 +16,16 @@ public class ValidateInput implements Input {
 
     @Override
     public int askInt(String question) {
-        int value;
-        while (true) {
+        boolean invalid = true;
+        int value = -1;
+        do {
             try {
-                value = Integer.parseInt(input.askStr(question));
-                break;
-            } catch (NumberFormatException e) {
-                output.println("Invalid input. Please enter a valid number.");
+                value = input.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                output.println("Ошибка: пожалуйста, введите число.");
             }
-        }
+        } while (invalid);
         return value;
     }
 }
