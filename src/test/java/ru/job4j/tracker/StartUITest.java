1c698a37c5dd18c6c9e7ef1769d86e807b9c45be
Konstantin <89169555954@mail.ru>
2025-02-19 00:04:49 +0300

    Репозитарий под проект Tracker

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StartUITest {
+    @Test
+    void whenInvalidExit() {
+        Output output = new StubOutput();
+        Input input = new MockInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction()
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln +
+                        "0. Завершить программу" + ln +
+                        "Ошибка: некорректный ввод. Введите число от 0 до 0" + ln +
+                        "Меню:" + ln +
+                        "0. Завершить программу" + ln +
+                        "=== Завершение программы ===" + ln
+        );
+    }
+}
+
+
+
+
+
