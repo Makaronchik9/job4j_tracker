cc5655f465c1d460fbcbd44a5aa3d9ce0991b1af
Konstantin <89169555954@mail.ru>
2025-04-22 00:13:38 +0300

    Репозитарий под проект Tracker

---
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,48 @@
+package ru.job4j.tracker;
+
+import java.util.Objects;
+
+public class Item {
+    private int id;
+    private String name;
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Item)) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id
+                && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{id=" + id + ", name='" + name + "'}";
+    }
+}
