34a0c3a89f603626c148d25ab0d7344e58a51406
Konstantin <89169555954@mail.ru>
2025-05-01 14:30:00 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/collection/PhoneDictionary.java
+++ src/main/java/ru/job4j/collection/PhoneDictionary.java
@@ -2,26 +2,22 @@ package ru.job4j.collection;
 
 import java.util.ArrayList;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<>();
+    private final ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
-        this.persons.add(person);
+        persons.add(person);
     }
 
     public ArrayList<Person> find(String key) {
-        Predicate<Person> getName = name -> name.getName().contains(key);
-        Predicate<Person> getPhone = name -> name.getPhone().contains(key);
-        Predicate<Person> getAddress = name -> name.getAddress().contains(key);
-        Predicate<Person> getSurname = name -> name.getSurname().contains(key);
-        var combine = getName.or(getPhone).or(getAddress).or(getSurname);
-        var result = new ArrayList<Person>();
-        for (Person person : persons) {
-            if (combine.test(person)) {
-                result.add(person);
-            }
-        }
-        return result;
+        var combine = (Predicate<Person>) p -> p.getName().contains(key)
+                || p.getPhone().contains(key)
+                || p.getAddress().contains(key)
+                || p.getSurname().contains(key);
+        return persons.stream()
+                .filter(combine)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
--- src/main/java/ru/job4j/collection/PriorityQueue.java
+++ src/main/java/ru/job4j/collection/PriorityQueue.java
@@ -3,20 +3,20 @@ package ru.job4j.collection;
 import java.util.LinkedList;
 
 public class PriorityQueue {
-     private LinkedList<Task> tasks = new LinkedList<>();
+    private final LinkedList<Task> tasks = new LinkedList<>();
 
-     public void put(Task task) {
-         var index = 0;
-         for (Task element : tasks) {
-             if (element.getPriority() > task.getPriority()) {
-                 break;
-             }
-             index++;
-         }
-         this.tasks.add(index, task);
-     }
+    public void put(Task task) {
+        var index = 0;
+        for (var element : tasks) {
+            if (element.getPriority() > task.getPriority()) {
+                break;
+            }
+            index++;
+        }
+        tasks.add(index, task);
+    }
 
-     public Task take() {
-         return tasks.poll();
-     }
+    public Task take() {
+        return tasks.poll();
+    }
 }
