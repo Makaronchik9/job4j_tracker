abfdc798fdb9f41e267017fe0038dd07f5012490
Konstantin <89169555954@mail.ru>
2025-05-14 00:33:01 +0300

    Репозитарий под проект Tracker

--- src/test/java/ru/job4j/collection/PriorityQueueTest.java
+++ src/test/java/ru/job4j/collection/PriorityQueueTest.java
@@ -1,26 +1,24 @@
 package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PriorityQueueTest {
-    @Test
-    public void whenHigherPrioritySecond() {
-        var queue = new PriorityQueue();
-        queue.put(new Task("low", 5));
-        queue.put(new Task("urgent", 1));
-        queue.put(new Task("middle", 3));
-        var result = queue.take();
-        assertThat(result.getDescription()).isEqualTo("urgent");
-    }
 
     @Test
-    public void whenHigherPriorityEquals() {
-        var queue = new PriorityQueue();
-        queue.put(new Task("low", 5));
-        queue.put(new Task("urgent", 5));
-        queue.put(new Task("middle", 5));
-        var result = queue.take();
-        assertThat(result.getDescription()).isEqualTo("low");
+    public void whenAddTasksThenTakeInPriorityOrder() {
+        PriorityQueue queue = new PriorityQueue();
+
+        Task task1 = new Task("Fix bug", "Fix critical bug", "Bug", 1);
+        Task task2 = new Task("Add feature", "Add new feature", "Feature", 2);
+        Task task3 = new Task("Update docs", "Update project documentation", "Documentation", 3);
+
+        queue.put(task2);
+        queue.put(task1);
+        queue.put(task3);
+
+        assertEquals(task1, queue.take());
+        assertEquals(task2, queue.take());
+        assertEquals(task3, queue.take());
     }
 }
