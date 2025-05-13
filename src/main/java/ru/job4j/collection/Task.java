9f10bdc164ab36d69ee804afc73567c3c556b01c
Konstantin <89169555954@mail.ru>
2025-05-14 00:25:19 +0300

    Репозитарий под проект Tracker

---
+++ src/main/java/ru/job4j/collection/Task.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+public class Task {
+    private final String name;
+    private final String description;
+    private final String type;
+    private final int priority;
+
+    public Task(String name, String description, String type, int priority) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
