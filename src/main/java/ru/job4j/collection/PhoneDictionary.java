ad185d8cabeaaab9d632b594d14abc9ef7708341
Konstantin <89169555954@mail.ru>
2025-12-24 00:27:20 +0300

    Репозитарий под проект Tracker

--- src/main/java/ru/job4j/collection/PhoneDictionary.java
+++ src/main/java/ru/job4j/collection/PhoneDictionary.java
@@ -1,30 +1,27 @@
 package ru.job4j.collection;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class PhoneDictionary {
-    private final List<Person> persons = new ArrayList<>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
-        persons.add(person);
+        this.persons.add(person);
     }
 
-    public List<Person> find(String key) {
-        Predicate<Person> byName = p -> p.getName().contains(key);
-        Predicate<Person> bySurname = p -> p.getSurname().contains(key);
-        Predicate<Person> byPhone = p -> p.getPhone().contains(key);
-        Predicate<Person> byAddress = p -> p.getAddress().contains(key);
-
-        Predicate<Person> combined = byName
-                .or(bySurname)
-                .or(byPhone)
-                .or(byAddress);
-
-        return persons.stream()
-                .filter(combined)
-                .collect(Collectors.toList());
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> getName = name -> name.getName().contains(key);
+        Predicate<Person> getPhone = name -> name.getPhone().contains(key);
+        Predicate<Person> getAddress = name -> name.getAddress().contains(key);
+        Predicate<Person> getSurname = name -> name.getSurname().contains(key);
+        Predicate<Person> combine = getName.or(getPhone).or(getAddress).or(getSurname);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
     }
 }
